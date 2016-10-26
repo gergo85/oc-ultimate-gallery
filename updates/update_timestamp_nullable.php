9aca396f2252ec80a495417215b5fb1610266711
Szabó Gergő <gergo85@gmail.com>
2016-10-26 08:36:43 +0200

    Minor improvement

--- updates/update_timestamp_nullable.php
+++ updates/update_timestamp_nullable.php
@@ -1,8 +1,7 @@
 <?php namespace Indikator\Gallery\Updates;
 
-use Schema;
-use DbDongle;
 use October\Rain\Database\Updates\Migration;
+use DbDongle;
 
 class UpdateTimestampsNullable extends Migration
 {
